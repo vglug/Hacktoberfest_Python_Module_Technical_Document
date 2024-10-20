6c5afbb1311a4be3e7abcde472c57fdb08f278df
asanm216anm216216170 <144462195+asanm216anm216216170@users.noreply.github.com>
2024-10-20 09:28:25 +0530

    Create Nesting.py

---
+++ Nesting.py
@@ -0,0 +1,15 @@
+number = 5
+
+# outer if statement
+if number >= 0:
+    # inner if statement
+    if number == 0:
+      print('Number is 0')
+    
+    # inner else statement
+    else:
+        print('Number is positive')
+
+# outer else statement
+else:
+    print('Number is negative')
