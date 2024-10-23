86962a82c19dd46d5b3cc901da6ead335dde38d5
Jayakumar velu <144428780+Jayaakumaar@users.noreply.github.com>
2024-10-23 12:35:32 +0530

    Update aera of circle.py

--- aera of circle.py
+++ aera of circle.py
@@ -1,14 +1,16 @@
-import math
-
+import math as m
+try:
 # Function to calculate the area of a circle
-def area_of_circle(radius):
-    return math.pi * radius ** 2
+    def area_of_circle(radius):
+        return m.pi * radius ** 2
 
 # Input radius from the user
-radius = float(input("Enter the radius of the circle: "))
+    radius = float(input("Enter the radius of the circle: "))
 
 # Calculate the area
-area = area_of_circle(radius)
+    area = area_of_circle(radius)
 
 # Display the result
-print(f"The area of the circle with radius {radius} is {area:.2f}")
+    print(f"The area of the circle with radius {radius} is {area:.2f}")
+except:
+    print(" please enter the integer value") 
