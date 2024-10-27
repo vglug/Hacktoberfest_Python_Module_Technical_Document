ec327ba8e1a30bca10a48b6c83295367189e03f5
gnanasegarant778 <144010170+gnanasegarant778@users.noreply.github.com>
2024-10-27 09:42:38 +0530

    Update Os path.py

--- Os path.py
+++ Os path.py
@@ -2,4 +2,8 @@ import os
 
 current_dir = os.getcwd()
 for filename in os.listdir(current_dir):
+    if os.path.isfile(os.path.join(current_dir, filename)):
+        print(f"File: {filename}")
+    else:
+        print(f"Directory: {filename}")
     
