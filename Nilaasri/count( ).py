cc5cad917845cfc0f6a6d5eab4ace103d983a270
NILAASRI <146331079+NILAASRI@users.noreply.github.com>
2024-10-29 09:30:10 +0530

    Create count( ).py

---
+++ Nilaasri/count( ).py
@@ -0,0 +1,22 @@
+#Count the letter 
+print("-------Count the letter-------")
+def countB(word):
+    print("Word = ",word)
+    count=0
+    for b in word:
+        if(b =='b'):
+            count=count+1
+    return count
+print("Number of 'b' =", countB("abbbabcdefbbghibka"))
+
+
+
+#Count the vowels
+print("-------Count the Vowels in a string-------")
+def countvowels(word):
+    print("Word =", word)
+    word=word.lower()
+    return{
+        v:word.count(v) for v in 'aeiou'
+        }  
+print(countvowels(" Python string is a data type used to represent a sequence of characters"))
